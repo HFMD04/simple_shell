f2ac3e69069aa87cdae3ee6653879368d0ea9d47
SMSB96 <saramiad96@gmail.com>
2023-09-12 14:14:16 -0700

    execute_cmd.c

---
+++ execute_cmd.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * execute_command - Execute a command using execve.
+ *
+ * @command: The command to execute.
+ */
+void execute_command(char *command)
+{
+	char *args[2];
+
+	args[0] = command;
+	args[1] = NULL;
+
+	if (execve(command, args, environ) == -1)
+	{
+		perror("Command execution error");
+		free(command);
+		exit(EXIT_FAILURE);
+	}
+	/* Free the command memory before calling execve */
+	free(command);
+}
