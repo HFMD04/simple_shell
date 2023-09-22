4bbd08165bdf786c4bed7af8cc241849a4a89e05
HFMD04 <hafsamiad2021@gmail.com>
2023-09-22 08:25:42 -0700

    updated

--- split_string.c
+++ split_string.c
@@ -12,38 +12,38 @@
 
 char **split_string(char *input, const char *delimiter, int *argc)
 {
-	char **tokens = NULL;
-	char *token;
-	int count = 0;
-	int i; /* Declare i outside of the loop */
+        char **tokens = NULL;
+        char *token;
+        int count = 0;
+        int i;
 
-	if (input == NULL || delimiter == NULL || argc == NULL)
-		return (NULL);
+        if (input == NULL || delimiter == NULL || argc == NULL)
+                return (NULL);
 
-	/* Count the number of tokens */
-	token = strtok(input, delimiter);
-	while (token != NULL)
-	{
-		tokens = realloc(tokens, sizeof(char *) * (count + 1));
-		if (tokens == NULL)
-		{
-			perror("Memory allocation error");
-			return (NULL);
-		}
-		tokens[count] = strdup(token);
-		if (tokens[count] == NULL)
-		{
-			perror("Memory allocation error");
-			for (i = 0; i < count; i++) /* Use the variable i here */
-				free(tokens[i]);
-			free(tokens);
-			return (NULL);
-		}
-		count++;
-		token = strtok(NULL, delimiter);
-	}
+        
+        token = strtok(input, delimiter);
+        while (token != NULL)
+        {
+                tokens = realloc(tokens, sizeof(char *) * (count + 1));
+                if (tokens == NULL)
+                {
+                        perror("Memory allocation error");
+                        return (NULL);
+                }
+                tokens[count] = strdup(token);
+                if (tokens[count] == NULL)
+                {
+                        perror("Memory allocation error");
+                        for (i = 0; i < count; i++)
+                                free(tokens[i]);
+                        free(tokens);
+                        return (NULL);
+                }
+                count++;
+                token = strtok(NULL, delimiter);
+        }
 
-	*argc = count;
-	return (tokens);
+        *argc = count;
+        return (tokens);
 
 }
